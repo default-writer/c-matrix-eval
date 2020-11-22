5e64b44c1ecf3703445ea3bc1f40bd7e26916e8e
Artur Mustafin <artur.mustafin@gmail.com>
2020-11-22 22:25:27 +0000

    solution

--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 
+void cmpxchng(int *a, int *b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
+}
+
+/*
+9.22. Дана матрица размером 4*4. Поменять первый элемент каждого столбца матрицы с максимальным элементом этого столбца, второй элемент этого столбца с минимальным элементом этого столбца. Вывести на печать заданную и образовавшуюся матрицы.
+*/
+
+void print_matrix(int M[4][4]) {
+  for (int i=0; i<4; i++) {
+    for (int j=0; j<4; j++) {
+      printf("%-4d", M[i][j]);
+    }
+    printf("\n");
+  }
+}
+
 int main(void) {
-  printf("Hello World\n");
+  int M[4][4]={{3,5,-1,2},{7,5,9,-3},{-1,7,4,3},{4,-5,8,-2}};
+  
+  print_matrix(M);
+
+  int* max_j = NULL;
+  int* min_j = NULL;
+  for (int i=0; i<4; i++) {
+    max_j = &M[0][i];
+    min_j = &M[1][i];
+    for (int j=0; j<4; j++) {
+      if (M[j][i] > *max_j) {
+        cmpxchng(max_j, &M[j][i]);
+      }
+      if (M[j][i] < *min_j) {
+        cmpxchng(min_j, &M[j][i]);
+      }
+    }
+  }
+  printf("\n");
+  print_matrix(M);
   return 0;
 }
